e4b7d753bdb4ef3c0bde4d2e2dcb590c1597b450
Richard Miller <richardmmill@gmail.com>
2022-08-21 15:07:36 -0400

    setup

---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+const Nav: Component = () => {
+    return (
+        <div><h2>Nav Component</h2><nav></nav></div>
+    )
+}
+
+export default Nav
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const Home: Component = () => {
+    return (
+        <div>
+            <h1>Home Component</h1>
+        </div>
+    )
+}
+
+export default Home
