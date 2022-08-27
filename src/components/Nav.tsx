5fa930039499df7dc67e9ea37904b09e168f1d74
Richard Miller <richardmmill@gmail.com>
2022-08-27 13:57:36 -0400

    dyynamic button repos count

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,12 @@
 import { NavLink } from "@solidjs/router";
 import { Component } from "solid-js";
+import { savedRepos } from "../pages/Saved";
 
 const Nav: Component = () => {
     return (
         <nav>
             <NavLink href="/" end activeClass="">Home</NavLink>
-            <NavLink href="/saved" activeClass="">Saved</NavLink>
+            <NavLink href="/saved" activeClass="">Saved - {savedRepos().length}</NavLink>
         </nav >
     )
 }
